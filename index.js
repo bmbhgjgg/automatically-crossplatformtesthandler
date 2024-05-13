eca9a9dc623926bb12b2db9747920fd117399d9a
bmbhgjgg <jsjcuris.mosans@hotmail.com>
2024-05-13 13:37:11 +0700

    update aptos project commit

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-function zigzagLevelOrder(root) {
+function levelOrder(root) {
   if (!root) return [];
   const result = [];
-  let isReverse = false;
   const queue = [root];
   while (queue.length) {
-    const size = queue.length;
-    const level = [];
-    for (let i = 0; i < size; i++) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
       const node = queue.shift();
-      if (isReverse) {
-        level.unshift(node.val);
-      } else {
-        level.push(node.val);
-      }
+      currentLevel.push(node.val);
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
-    result.push(level);
-    isReverse = !isReverse;
+    result.push(currentLevel);
   }
   return result;
 }
